691b734649e003c5be24a39eed7828da7687347c
guillermo <guillermo@guillermo-PC>
2018-07-24 23:51:31 -0400

    Added testing enviroment to application.

--- garrus-frontend/src/environments/environment.prod.ts
+++ garrus-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const restBaseUrl = 'http://ec2-54-213-224-158.us-west-2.compute.amazonaws.com:8080/'
+export const restBaseUrl = 'http://ec2-18-236-178-178.us-west-2.compute.amazonaws.com:8080/'
 
 export const environment = {
   production: true
