1565b312dafc5f19100adb517bd59ec48e451db0
konum <ggvalcarce@gmail.com>
2018-08-13 11:12:34 -0300

    Missing commit stuff.

--- garrus-frontend/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ garrus-frontend/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -3,13 +3,6 @@ import {Routes} from '@angular/router';
 
 import {DashboardComponent} from '../../dashboard/dashboard.component';
 import {UserProfileComponent} from '../../user-profile/user-profile.component';
-import {TableListComponent} from '../../table-list/table-list.component';
-import {TypographyComponent} from '../../typography/typography.component';
-import {IconsComponent} from '../../icons/icons.component';
-import {MapsComponent} from '../../maps/maps.component';
-import {NotificationsComponent} from '../../notifications/notifications.component';
-import {UpgradeComponent} from '../../upgrade/upgrade.component';
-
 export const AdminLayoutRoutes: Routes = [
   {
     path: '',
@@ -19,22 +12,8 @@ export const AdminLayoutRoutes: Routes = [
       path: 'vehicles',
       loadChildren: 'app/vehicles/vehicles.module#VehiclesModule'
     }]
-  },{
-    path: '',
-//    canActivate: [AuthGuard],
-//    canActivateChild: [AuthGuard],
-    children: [{
-      path: 'users',
-      loadChildren: 'app/admin-users/admin-users.module#AdminUsersModule'
-    }]
   },
   {path: 'dashboard', component: DashboardComponent},
   {path: 'user-profile', component: UserProfileComponent},
   {path: 'user-profile', component: UserProfileComponent},
-  {path: 'table-list', component: TableListComponent},
-  {path: 'typography', component: TypographyComponent},
-  {path: 'icons', component: IconsComponent},
-  {path: 'maps', component: MapsComponent},
-  {path: 'notifications', component: NotificationsComponent},
-  {path: 'upgrade', component: UpgradeComponent},
 ];
